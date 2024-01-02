019612af1432217003024f3121b1c350f2314e41
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2024-01-02 16:32:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 // Complete the js code
-function Car(make, model) {}
-
-function SportsCar(make, model, topSpeed) {}
-
+function Car(make, model) {
+	this.make=make;
+	this.model=model;
+}
+Car.prototype.getMakeModel= function(){
+	return `${this.make} ${this.model}`;
+}
+function SportsCar(make, model, topSpeed) {
+	this.make= make;
+	this.model= model;
+	this.topSpeed= topSpeed
+}
+SportsCar.prototype._proto_= Car.prototype;
+SportsCar.prototype.getTopSpeed= function(){
+	return this.topSpeed;
+}
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
